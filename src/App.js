783ae4800486c13242e590dbfffdff8755fb9f1f
Omkar Ghate <otg0231@gmail.com>
2021-08-07 22:06:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
-
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 export default function App() {
+  const [inputFields, setInputFields] = useState([
+    { id: uuidv4(), firstName: "" }
+  ]);
+
+  var [count, setCount] = useState(1);
+
+  const handleAddFields = (e) => {
+    e.preventDefault();
+    setCount(count + 1);
+
+    if (count < 7) {
+      setInputFields([...inputFields, { id: uuidv4(), firstName: "" }]);
+    }
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <form>
+        <button className="addBtn" onClick={() => handleAddFields(event)}>
+          Add
+        </button>
+        {inputFields.map((inputField) => (
+          <div key={inputField.id}>
+            <label>Name</label>
+            <input id="name" type="text" placeholder="Enter your name" />
+          </div>
+        ))}
+      </form>
     </div>
   );
 }
